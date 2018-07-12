57a36660c5291e9d40b8d96beb62c68104c568ca
xumingquan <xumingquan@yidian-inc.com>
2018-07-12 17:58:10 +0800

    1.0.0版本

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import  "./css/common.css";
 import Layer from './components/layer/layer';
 const App = function(){
     var dom = document.getElementById("app");
---
+++ src/views/app1.js
@@ -0,0 +1,3 @@
+var test = {};b = Object.prototype;
+
+
